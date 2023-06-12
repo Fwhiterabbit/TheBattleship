2165a3571e8af180e76d9c7e8245a9c5db20c4e1
Fwhiterabbit <thisismichael91@gmail.com>
2023-06-12 19:13:49 +0000

    updated comments section

--- run.py
+++ run.py
@@ -8,21 +8,23 @@ Hidden_Pattern = [[" "] * BOARD_SIZE for _ in range(BOARD_SIZE)]
 Guess_Pattern = [[" "] * BOARD_SIZE for _ in range(BOARD_SIZE)]
 Previous_Guesses = []
 
-"""
+
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+    """
 Clears the console screen, is uses "os" module
 and thte system function to call the appropriate
 command based on the operating system
-"""
-def clear_screen():
-    os.system("cls" if os.name == "nt" else "clear")
+    """
 
-"""
+
+def print_board(board):
+    """
 This function prints the game the game board
 with the current state of the hidden
 and guessed patterns, it displays the row and
 collumn labels along with the corresponding cells.
-"""
-def print_board(board):
+    """
     clear_screen()
     print("    .+*^*+. The Game Board .+*^*+.")
     print("     A   B   C   D   E   F   G   H")
@@ -34,26 +36,28 @@ def print_board(board):
             print("   ├───┼───┼───┼───┼───┼───┼───┼───┤")
     print("   └───┴───┴───┴───┴───┴───┴───┴───┘")
 
-"""
+
+def get_ship_location():
+    """
 This function prompts the player to enter a 
 guess for the location of a battleship.
 It validates the input and returns the row 
 and column indicates of thte guess.
-"""
-def get_ship_location():
+    """
     while True:
         guess = input("Enter a guess (e.g., 4B): ").upper()
         if len(guess) == 2 and guess[0] in "12345678" and guess[1] in "ABCDEFGH":
             return int(guess[0]) - 1, ord(guess[1]) - ord("A")
         print("Please enter a valid guess.")
 
-"""
+
+def create_ships(board):
+    """
 This function randomly places battleships on the
 hidden patter board, is uses "randint" function
 to determine the size, direction, and position of 
 each battleship.
-"""
-def create_ships(board):
+    """
     ship_sizes = [4, 3, 2, 1]
     for size in ship_sizes:
         ship_placed = False
@@ -75,49 +79,54 @@ def create_ships(board):
                         board[ship_r + i][ship_cl] = "■"
                     ship_placed = True
 
-"""
+
+def count_hit_ships(board):
+    """
 This function counts the number
 of hits (battleship) on the board
-"""
-def count_hit_ships(board):
+    """
     hit_count = 0
     for row in board:
         hit_count += row.count("■")
     return hit_count
 
-"""
-This function calculate the accuracy of the players guesses
-"""
+
 def calculate_accuracy(guesses):
+    """
+This function calculate the accuracy of the players guesses
+    """
     total_guesses = 20
     hits = guesses.count("X")
     accuracy = (hits / total_guesses) * 100
     return accuracy
 
-"""
+
+def play_again():
+    """
 This functio allow player to return to the main menu
 and start again
-"""
-def play_again():
+    """
     print(f"Thank you for playing Battleship, {username}")
     answer = input("To go back to the main menu just hit Enter").lower()
     return answer == "yes" or "y"
 
-"""
+
+def welcome_screen():
+    """
 This funcion prinst slowly welcome message
 to the player before main menu
-"""
-def welcome_screen():
+    """
     welcome_message = "Welcome to the Battleship Game!"
     for char in welcome_message:
         print(char, end="", flush=True)
         time.sleep(0.1)
     print("\nLet's get ready to sink some battleships!\n")
 
-"""
-This function prints main menu
-"""
+
 def print_welcome():
+    """
+This function prints main menu
+    """
     clear_screen()
     print("-----------------------")
     print("|  The Battleship  |")
@@ -131,10 +140,11 @@ def print_welcome():
     print(f"  -->   {username}   <--")
     print("-----------------------")
 
-"""
-This function prints rules of the game
-"""
+
 def print_rules():
+    """
+This function prints rules of the game
+    """
     clear_screen()
     print("-----------------------")
     print("|       RULES       |")
@@ -154,10 +164,11 @@ def print_rules():
     print("-----------------------")
     input("Press Enter to go back to the main menu.")
 
-"""
-This function check accuracy of players hit
-"""
+
 def print_outro(accuracy):
+    """
+This function check accuracy of players hit
+    """
     clear_screen()
     print("-----------------------")
     print("|     GAME OVER     |")
@@ -165,11 +176,12 @@ def print_outro(accuracy):
     print("    Accuracy: {:.2f}%".format(accuracy))
     print("-----------------------")
 
-"""
+
+def print_previous_guesses():
+    """
 This dunction printing out up to 3
 previous guesses 
-"""
-def print_previous_guesses():
+    """
     if Previous_Guesses:
         print("Previous Guesses:")
         num_guesses = min(len(Previous_Guesses), 3)
@@ -178,10 +190,11 @@ def print_previous_guesses():
     else:
         print("No previous guesses.")
 
-"""
-This fucntion taking user username
-"""
+
 def get_username():
+    """
+This fucntion taking user username
+    """
     welcome_screen()
     time.sleep(2)
     username = input("Enter your username: ")
